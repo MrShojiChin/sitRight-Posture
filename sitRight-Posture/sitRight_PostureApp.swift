84eccc2bb0fd88b2a55fbdb7fa29386c343fcc98
Chinnapat Premudomkit <117561156+MrShojiChin@users.noreply.github.com>
2025-09-07 11:36:09 +0700

    Initial Commit

---
+++ sitRight-Posture/sitRight_PostureApp.swift
@@ -0,0 +1,17 @@
+//
+//  sitRight_PostureApp.swift
+//  sitRight-Posture
+//
+//  Created by Ryu on 7/9/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct sitRight_PostureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
